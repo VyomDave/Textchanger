04dd7d762dd377fb6522fac5044ed1f074214117
VyomDave <67219666+VyomDave@users.noreply.github.com>
2021-02-16 14:42:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0
+nosey=0
+difference=0
+rightwristx=0
+leftwristx=0
+
 function preload(){
 }
 
@@ -13,6 +19,10 @@ canvas.position(550,170)
 }
 function draw(){
   background("#7FFF00")
+  fill("#ff0000")
+  stroke("red")
+  text("Vyom" , nosex, nosey)
+  textSize(difference)
 }
 function modelloaded(){
     console.log("pose net is working do not worry!!!")
@@ -20,5 +30,11 @@ function modelloaded(){
 function gotPoses(results) {
     if (results.length > 0){
         console.log(results)
+        nosex = results[0].pose.nose.x
+        nosey = results[0].pose.nose.y
+        console.log("nosex ="+nosex+ "nosey ="+nosey)
+        leftwristx = results[0].pose.leftWrist.x
+        rightwristx=results[0].pose.rightWrist.x
+        difference = floor(leftwristx - rightwristx)
     }
 }
